d4a52709d68a4c5ae8d37fdaffd8e212186df08b
B-e-sa <joaospereira963@gmail.com>
2024-01-28 22:04:40 -0300

    feat: adding delete method to answers repository

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -1,5 +1,7 @@
 import { Answer } from '../../enterprise/entities/answer'
 
 export interface AnswersRepository {
-  create(answer: Answer): Promise<void>
+  create(answer: Answer): Promise<Answer | null>
+  delete(answer: Answer): Promise<Answer | null>
+  findById(id: string): Promise<Answer | null>
 }
