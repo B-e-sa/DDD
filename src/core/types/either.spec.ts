4fd3c457dd6a3551a8737da830b42fa385d82d1c
B-e-sa <joaospereira963@gmail.com>
2024-02-03 15:44:13 -0300

    update: remove explicit any types from either class

--- src/core/types/either.spec.ts
+++ src/core/types/either.spec.ts
@@ -1,23 +1,21 @@
-import { Either, left, right } from "./either"
+import { Either, left, right } from './either'
 
 function doSomething(shouldSuccess: boolean): Either<string, string> {
-    return shouldSuccess
-        ? right('success')
-        : left('error')
+  return shouldSuccess ? right('success') : left('error')
 }
 
 test('success result', () => {
-    const success = doSomething(true)
+  const success = doSomething(true)
 
-    if (success.isRight()) {
-        console.log(success.value)
-    }
+  if (success.isRight()) {
+    console.log(success.value)
+  }
 
-    expect(success.isRight()).toBeTruthy()
+  expect(success.isRight()).toBeTruthy()
 })
 
 test('error result', () => {
-    const error = doSomething(false)
+  const error = doSomething(false)
 
-    expect(error.isLeft()).toBeTruthy()
+  expect(error.isLeft()).toBeTruthy()
 })
